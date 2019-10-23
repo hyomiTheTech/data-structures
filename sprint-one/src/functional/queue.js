762b6bf9e2e3def9e3a4f29895c554c5de9873f1
Eui Hyun Kim <hyomithetech@gmail.com>
2019-10-23 15:35:33 -0700

    Finish the functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,37 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var size = 0;
+
+  var deletedVal = [];
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size++;
+    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+    } else {
+      size = 0;
+    }
+
+    deletedVal.push(storage[1]);
+    delete storage[1];
+
+    for (var i = 1; i <= size; i++) {
+      var previousNum = i + 1;
+      storage[i] = storage[previousNum];
+    }
+
+    return deletedVal.pop();
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
