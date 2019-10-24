498ba643c42b51d366524ce69f68941d5f3f6969
Eui Hyun Kim <hyomithetech@gmail.com>
2019-10-23 22:01:35 -0700

    Finish the functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
+  var stack = {};
+
+  stack.storage = {};
+
+  stack.count = 0;
+
+  _.extend(stack, stackMethods);
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
 
+  pop: function() {
+    if (this.count > 0) {
+      this.count--;
+    } else {
+      this.count = 0;
+    }
 
+    return this.storage[this.count + 1];
+    delete this.storage[this.count + 1];
+  },
+
+  size: function() {
+    return this.count;
+  }
+};
