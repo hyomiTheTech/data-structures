729c897e120ffe300f2a0073d2c5009a42a16664
Eui Hyun Kim <hyomithetech@gmail.com>
2019-10-24 13:38:28 -0700

    Finish the es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,35 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+    this.deletedVal = [];
   }
 
+  enqueue(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  dequeue() {
+    if (this.count > 0) {
+      this.count--;
+    } else {
+      this.count = 0;
+    }
+
+    this.deletedVal.push(this.storage[1]);
+    delete this.storage[1];
+
+    for (var i = 1; i <= this.count; i++) {
+      var previousNum = i + 1;
+      this.storage[i] = this.storage[previousNum];
+    }
+
+    return this.deletedVal.pop();
+  }
+
+  size() {
+    return this.count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  }
+
+  pop() {
+    if (this.count > 0) {
+      this.count--;
+    } else {
+      this.count = 0;
+    }
+
+    return this.storage[this.count + 1];
+    delete this.storage[this.count + 1];
+  }
+
+  size() {
+    return this.count;
+  }
 }
