68d700361cad6c573bf1bbd7d40ebc4bd6bb11f5
Eui Hyun Kim <hyomithetech@gmail.com>
2019-10-25 15:46:39 -0700

    Finish the tree assignment

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +13,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    var result = false;
+    for (var i = 0; result === false && i < this.children.length; i++) {
+      result = this.children[i].contains(target);
+    }
+    return result;
+  }
+  return false;
 };
 
-
-
+// var giveChild = function(tree, value) {
+//   if (tree.children.length === 0) {
+//     tree.children.push(tree);
+//   } else {
+//     giveChild(tree.children, value);
+//   }
+// };
 /*
  * Complexity: What is the time complexity of the above functions?
  */
