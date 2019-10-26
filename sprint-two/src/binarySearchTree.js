0a423cbc648ebc6663f292cb9475256393ebd09e
Eui Hyun Kim <hyomithetech@gmail.com>
2019-10-26 12:17:23 -0700

    Finish BinarySearchTree assignment

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,48 @@
 var BinarySearchTree = function(value) {
+  var newClass = Object.create(tree);
+  newClass.left = null;
+  newClass.right = null;
+  newClass.value = value;
+  return newClass;
 };
-
+var tree = {};
+tree.insert = function(value) {
+  if (value > this.value) {
+    if (this.right) {
+      return this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  } else {
+    if (this.left) {
+      return this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  }
+};
+tree.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else {
+    if (this.left && value < this.value) {
+      return this.left.contains(value);
+    } else if (this.right && value > this.value) {
+      return this.right.contains(value);
+    } else {
+      return false;
+    }
+  }
+};
+tree.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
