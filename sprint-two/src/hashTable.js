ee801c04dbd5c049580417b8fec2611aaae708d5
Eui Hyun Kim <hyomithetech@gmail.com>
2019-10-27 20:24:55 -0700

    Finish the hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,32 +4,62 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKeys(k, this._limit);
+  this._storage[index] = [];
+  var bucket = this._storage[index];
   var tuple = [k, v];
-  this._storage.set(index, tuple);
+
+  if (Array.isArray(bucket[0]) === true) {
+    for (var i = 0; i < bucket.length; i++) {
+      var tuples = bucket[i];
+      if (tuples[0] === k) {
+        tuples[1] = v;
+      }
+    }
+  } else {
+    bucket.push(tuple);
+  }
+
+  // receive an index and push a bucket in the index we get from the getIndexBelowMax.
+
+  // if the index has bucket, we reassign the value,
+
+  // else we push the tuple.
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index, k);
+  var index = getIndexBelowMaxForKeys(k, this._limit);
+  var bucket = this._storage[index];
+
+  // iterate over the bucket
+  for (var i = 0; i < bucket.length; i++) {
+    var tuples = bucket[i];
+    if (tuples[0] === k) {
+      return tuples[1];
+    } else {
+      return undefined;
+    }
+  }
+
+  // check to see if tuples[0] is equal k
+  // if yes return tuples[1]
+  // if can't find it return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function(tuples, index, storage) {
-    if (Array.isArray(tuples)) {
-      for (var i = 0; i < tuples.length; i++) {
-        var tuple = tuples[i];
-        if (tuple[0] === k) {
-          console.log(_.without(tuples, tuple));
-          return storage[index] = _.without(tuples, tuple);
-        }
-      }
+  var index = getIndexBelowMaxForKeys(k, this._limit);
+  var bucket = this._storage[index];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuples = bucket[i];
+    if (tuples[0] === k) {
+      bucket.splice(i, 1);
+      this._storage[index] = bucket;
     }
-  });
+  }
+  // iterate over the bucket,
+  // if tuple[0] === k
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
